5a53485f67936f64c21cd3dac7893aed53895720
carlosmacho <carlosmacho@ipvc.pt>
2023-03-03 12:28:09 +0000

    Aula PL 03/03/2023
    Ciclos de Vida
    Strings / Translations

--- app/src/main/java/ipvc/estg/git/MainActivity.kt
+++ app/src/main/java/ipvc/estg/git/MainActivity.kt
@@ -2,11 +2,16 @@ package ipvc.estg.git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MY_TAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
 
         // adicionei o metodo A
 
@@ -15,5 +20,39 @@ class MainActivity : AppCompatActivity() {
         // john added method C
 
         // Carlos added method D
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    fun funcaoClick(view: View) {
+        Toast.makeText(this, R.string.key_button_name, Toast.LENGTH_SHORT).show()
     }
 }
