f84a0c4c07e6040b1c3ab522ba0c61414b370ed3
Carlos Macho <carlosmacho@ipvc.pt>
2023-04-26 15:47:21 +0100

    Exame Prático - Exercicios 24/04/2023

--- app/src/main/java/ipvc/estg/git/MainActivity.kt
+++ app/src/main/java/ipvc/estg/git/MainActivity.kt
@@ -1,11 +1,14 @@
 package ipvc.estg.git
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -17,6 +20,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate")
+
+        val sharedPref: SharedPreferences = getSharedPreferences(getString(R.string.preference_file_key),
+            Context.MODE_PRIVATE)
+
+        // Retrieve the sound value from the shared preferences and set the checkbox accordingly
+        val tickedValue = sharedPref.getBoolean(getString(R.string.ticked), false)
+        Log.d("****SHAREDPREF", "Read $tickedValue")
+
+        if(tickedValue){
+            findViewById<CheckBox>(R.id.checkbox1).isChecked = tickedValue
+        }
 
         // adicionei o metodo A
 
@@ -63,21 +77,51 @@ class MainActivity : AppCompatActivity() {
 
     fun onClick(view: View) {
 
-       /* val textView = findViewById<TextView>(R.id.tv1);
+        //val textView = findViewById<TextView>(R.id.tv1);
 
         val text1 = findViewById<EditText>(R.id.et1)
         val text2 = findViewById<EditText>(R.id.et2)
         val text3 = findViewById<EditText>(R.id.et3)
 
-        val result = text1.text.toString().toInt() * text2.text.toString().toInt() + text3.text.toString().toInt();
+        var result = (text1.text.toString().toInt() + text2.text.toString().toInt()) * text3.text.toString().toInt();
 
-        textView.setText("O resultado é $result")*/
+        if(result > 30 && findViewById<CheckBox>(R.id.checkbox1).isChecked == false){
+            Toast.makeText(
+                applicationContext,
+                "Resultado: $result",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        else if ( findViewById<CheckBox>(R.id.checkbox1).isChecked && result > 30 ){
+            result *= 2
+            Toast.makeText(
+                applicationContext,
+                "Resultado: $result",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        Log.d(TAG, "Resultado da conta: $result")
+
+        //textView.setText("O resultado é $result")
 
 
-        val intent = Intent (this, MainActivity2::class.java)
-        intent.putExtra(PARAM1_NAME, findViewById<EditText>(R.id.et1).text.toString())
-        startActivityForResult(intent, ACT2_CODE)
+//        val intent = Intent (this, MainActivity2::class.java)
+//        intent.putExtra(PARAM1_NAME, findViewById<EditText>(R.id.et1).text.toString())
+//        startActivityForResult(intent, ACT2_CODE)
 
+    }
+
+    fun onCheck(view: View) {
+        if (view is CheckBox) {
+            val sharedPref: SharedPreferences = getSharedPreferences(getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE)
+            with (sharedPref.edit()) {
+                putBoolean(getString(R.string.ticked), view.isChecked)
+                commit()
+            }
+            Log.d("****SHAREDPREF", "Changed to ${view.isChecked}")
+
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -96,4 +140,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 }
